db964163a870a2c2b29fb794e825b50b0aa82da3
David Mawdsley <david.mawdsley@manchester.ac.uk>
2017-02-01 16:03:29 +0000

    Add uniqueness tests for annotations
    (Should probably go in package code itself, since really testing the data, not the code)

--- tests/testthat/testSpotTheDifference.R
+++ tests/testthat/testSpotTheDifference.R
@@ -1,14 +1,34 @@
 context("Test spot the difference data")
 
-test_that("We have a time for every row", {
-
+test_that("We can load the data", {
+  
   attentions <- loadSpotTheDifference("~/IDInteraction/spot_the_difference/attention/")
   expect_equal(sum(is.na(attentions$attTransMidss)),0)
-  attentionsrecode <- loadSpotTheDifference("~/IDInteraction/spot_the_difference/attention/",
-                                      keyfile="~/IDInteraction/spot_the_difference/controlfiles/transitionAnnotations.csv")
   
+  attentionsrecode <- loadSpotTheDifference("~/IDInteraction/spot_the_difference/attention/",
+                                            keyfile="~/IDInteraction/spot_the_difference/controlfiles/transitionAnnotations.csv")
   expect_equal(sum(is.na(attentionsrecode$attTransMidss)),0)
   
+  
+  expect_equal(length(unique(attentions$eventtype)), 2)
+  expect_equal(length(unique(attentionsrecode$eventtype)), 3)
+  
+  # Expect each type of annotation to be unique in time
+  for(p in unique(attentions$participantCode)){
+    for(a in unique(attentions$eventtype)){
+      thisp <- attentions[attentions$participantCode==p & attentions$eventtype==a,]
+      expect_equal(length(unique(thisp$attTransMidss)), length(thisp$attTransMidss), info=paste(p,a))
+    }
+  }
+  
+  for(p in unique(attentionsrecode$participantCode)){
+    for(a in unique(attentionsrecode$eventtype)){
+      thisr <- NULL
+      thisr <- attentionsrecode[attentionsrecode$participantCode==p & attentionsrecode$eventtype == a,]
+      #print(paste(p, a,length(unique(thisr$attTransMidss)), length(thisr$attTransMidss)))
+      expect_equal(length(unique(thisr$attTransMidss)), length(thisr$attTransMidss), info=paste(p,a))
+    }
+  }
   
   
 })
